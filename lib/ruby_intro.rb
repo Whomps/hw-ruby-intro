cc1d489107fc1cbaf195afd4d827e8fa8c963598
EC2 Default User <ec2-user@ip-172-31-37-115.us-east-2.compute.internal>
2018-02-14 02:57:54 +0000

    Progress push - 2 functions unimplemented

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -17,16 +17,33 @@ end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
+  if arr.combination(2).detect{|a,b| a+b == n} != nil
+    return true
+  else
+    return false
+  end
+  #
 end
 
 # Part 2
 
 def hello(name)
   # YOUR CODE HERE
+  "Hello, #{name}"
+  #
 end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
+#  if s[0] == [^aeiou\W]/i 
+#    return false
+#  else
+#    return true
+#  end
+  #
+  
+  s =~ /^[^aeiou\W]/i
+  
 end
 
 def binary_multiple_of_4? s
